38e61aff329795b2b9d5cf064ae97eb3dfc304a8
hakobyaann <109814279+hakobyaann@users.noreply.github.com>
2023-03-01 18:45:58 +0400

    Add files via upload

---
+++ Multiton.cpp
@@ -0,0 +1,38 @@
+#include "Multiton.h"
+
+int MyClass::size()
+{
+    return vec.size();
+}
+
+inline MyClass& MyClass::CreateObject()
+{
+    {
+        if (vec.empty())
+        {
+            vec.push_back(new MyClass());
+            ++count;
+            return *vec[count - 1];
+        }
+        else if (vec.size() < MaxObjects && count != MaxObjects)
+        {
+            vec.push_back(new MyClass());
+            ++count;
+            return *vec[count - 1];
+        }
+        else if (count == MaxObjects)
+        {
+            count = 0;
+        }
+        ++count;
+        return *vec[count - 1];
+    }
+}
+inline std::string MyClass::GetName() const
+{
+    return name;
+}
+inline void MyClass::SetName(const std::string& n)
+{
+    name = n;
+}
