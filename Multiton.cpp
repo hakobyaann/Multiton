c85e8f3d7e153ca175710408047545675c71f398
hakobyaann <109814279+hakobyaann@users.noreply.github.com>
2023-03-01 18:49:00 +0400

    Update Multiton.cpp

--- Multiton.cpp
+++ Multiton.cpp
@@ -36,3 +36,30 @@ inline void MyClass::SetName(const std::string& n)
 {
     name = n;
 }
+
+int main()
+{
+    MyClass& obj1 = MyClass::CreateObject();
+    MyClass& obj2 = MyClass::CreateObject();
+    MyClass& obj3 = MyClass::CreateObject();
+    MyClass& obj4 = MyClass::CreateObject();
+    MyClass& obj5 = MyClass::CreateObject();
+    MyClass& obj6 = MyClass::CreateObject();
+    MyClass& obj7 = MyClass::CreateObject();
+    obj1.SetName("Thomas");
+    obj2.SetName("Anna");
+    obj3.SetName("Lily");
+    obj4.SetName("Mary");
+    obj7.SetName("Harry");
+   std::cout << obj1.GetName() << std::endl;
+   std::cout << obj2.GetName() << std::endl;
+   std::cout << obj3.GetName() << std::endl;
+   std::cout << obj4.GetName() << std::endl;
+   std::cout << obj5.GetName() << std::endl;
+   std::cout << obj6.GetName() << std::endl;
+   std::cout << obj7.GetName() << std::endl;
+   std::cout << obj4.GetName() << std::endl;
+   std::cout << obj5.GetName() << std::endl;
+   std::cout << obj6.GetName() << std::endl;
+   std::cout << obj7.GetName() << std::endl;
+}
